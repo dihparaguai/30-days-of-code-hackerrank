7d13f3691d6b30e339390bcded0c9f21b2342f4b
Diego Paraguai <107950626+dihparaguai@users.noreply.github.com>
2024-01-31 09:11:54 -0300

    Day 9: Factorial - 30 Days of Code

---
+++ Factorial.cs
@@ -0,0 +1,61 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+/*
+Sample Input
+3
+
+Sample Output
+6
+*/
+class Result
+{
+
+    /*
+     * Complete the 'factorial' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int factorial(int n)
+    {
+        if (n < 3) // se n menor que 3, ja devolve o n
+        {
+            return n; 
+        }
+        else 
+        {
+            return factorial(n-1) * n ; // chama a funcao recursivamente, passando n-1, e cada retorno sera multiplicado pelo n atual da instancia da recursao
+        }
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        int result = Result.factorial(n);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
